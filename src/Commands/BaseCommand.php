64a9fe23527e29275584658f49263a82249a10f0
Clemens Tolboom <clemens@build2be.com>
2014-07-26 17:09:55 +0200

    Fixed some logggin due to verbosity.

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -3,6 +3,7 @@
 namespace PhpGrade\Commands;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class BaseCommand extends Command
 {
@@ -12,4 +13,16 @@ class BaseCommand extends Command
         parent::__construct($name);
     }
 
+    /**
+     * log based on log level given and current verbosity level.
+     *
+     * @param OutputInterface $output
+     * @param $text
+     * @param int $level
+     */
+    public function log(OutputInterface $output, $text, $level = 1) {
+          if ($output->getVerbosity() > $level) {
+            $output->writeln("$level: $text");
+          }
+      }
 }
